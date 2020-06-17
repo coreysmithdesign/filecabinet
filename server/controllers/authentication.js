2c5859f2ffaf780730d2d549bb375ee781d58a75
Corey Smith <corey@coreysmith.design>
2020-06-17 12:28:56 -0700

    test endpoints

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,7 +1,13 @@
 const bcrypt = require("bcrypt");
 
 module.exports = {
-  register: (req, res) => {},
-  login: (req, res) => {},
-  logout: (req, res) => {},
+  register: (req, res) => {
+    res.status(200).send("register sup");
+  },
+  login: (req, res) => {
+    res.status(200).send("login sup");
+  },
+  logout: (req, res) => {
+    res.status(200).send("logout sup");
+  },
 };
